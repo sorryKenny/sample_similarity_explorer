31e90aa7825fc78b13008f07e81e03345d64028b
Kenny <chenanyikenny@gmail.com>
2026-02-04 02:01:48 -0500

    Add test for compute_sample_similarity function

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,1 +1,17 @@
+import pandas as pd
+from sample_similarity_explorer.similarity import compute_sample_similarity
+
+
+def test_similarity_shape():
+    df = pd.DataFrame(
+        {
+            "S1": [1, 2, 3],
+            "S2": [1, 2, 3],
+            "S3": [3, 2, 1],
+        },
+        index=["G1", "G2", "G3"],
+    )
+
+    sim = compute_sample_similarity(df, metric="pearson")
+    assert sim.shape == (3, 3)
 

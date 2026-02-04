9520eec569ce6cc93fbc8625ecccaa06bfb83001
Kenny <chenanyikenny@gmail.com>
2026-02-04 02:00:50 -0500

    Implement load_feature_matrix function
    Added a function to load feature matrices from CSV or TSV files.

--- src/sample_similarity_explorer/io.py
+++ src/sample_similarity_explorer/io.py
@@ -1,1 +1,17 @@
+import pandas as pd
+
+
+def load_feature_matrix(path: str) -> pd.DataFrame:
+    """
+    Load a feature-by-sample matrix from CSV or TSV.
+    """
+    if path.endswith(".tsv"):
+        df = pd.read_csv(path, sep="\t", index_col=0)
+    else:
+        df = pd.read_csv(path, index_col=0)
+
+    if df.empty:
+        raise ValueError("Input matrix is empty.")
+
+    return df
 
